cc8eca0b6ecdc9614409aa77dd08ef9884704cf8
Divanchik <d-sh-2016@list.ru>
2022-06-28 21:24:01 +0400

    Custom fonts allowed, less settings

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import pygame as pg
+import argparse as argp
 import sys
 import json
 import os
 
-# load config file
-with open("settings.json") as f:
-    config = json.load(f)
 
-# print fonts
-if config["printfonts"]:
+parser = argp.ArgumentParser()
+parser.add_argument(
+    "--logfonts",
+    action='store_true',
+    help="write all accesible fonts to text file")
+parser.add_argument(
+    "--nocycle",
+    action='store_true',
+    help="close program after showing all lines")
+args = parser.parse_args()
+
+
+if args.logfonts:
     fontlist = pg.font.get_fonts()
     fontlist.sort()
-    with open("fonts.txt", 'w') as f:
+    with open("fonts.log", 'w') as f:
         for i in fontlist:
             f.write(i + '\n')
+
+
+with open("settings.json") as f:
+    config = json.load(f)
 
 # get filenames
 file_list = []
@@ -33,21 +46,27 @@ else:
 
 # load first file
 with open(os.path.join(config["folderpath"], file_list[0])) as f:
-    sub_text = f.read()
-    sub_text = sub_text.replace("\n", " ")
+    sub_lines = f.read().split("\n\n")
+for i in range(len(sub_lines)):
+    sub_lines[i] = sub_lines[i].replace('\n', '')
 
 FPS = 60
 SIZE = (config["width"], config["height"])
+BG_COLOR = (240, 240, 240)
+FG_COLOR = (100, 100, 255)
 
 pg.init()
 sc = pg.display.set_mode(SIZE)
 pg.display.set_caption("LiveSubtitles by DimaDivan")
 clock = pg.time.Clock()
 
-ls_font = pg.font.SysFont("arial", config["fontsize"])
-ls_text = ls_font.render(sub_text, True, (200, 200, 200))
+# ls_font = pg.font.SysFont("arial", config["fontsize"])
+ls_font = pg.font.Font(config['customfonts'][0], config['fontsize'])
 
-x = SIZE[0] - 1
+
+ind = 0
+ls_text = ls_font.render(sub_lines[ind], True, FG_COLOR)
+x = SIZE[0]
 y = (SIZE[1] - ls_text.get_height())//2
 
 while True:
@@ -58,12 +77,26 @@ while True:
             sys.exit()
 
     # --------
-    sc.fill((0, 0, 0))
+
+    # draw
+    sc.fill(BG_COLOR)
     sc.blit(ls_text, (x, y))
+
+    # shift text
     if x > -ls_text.get_width():
         x -= config["speed"]
+    
+    # change text
     if x <= -ls_text.get_width():
-        x = SIZE[0] - 1
+        x = SIZE[0]
+        if args.nocycle:
+            ind += 1
+            if ind == len(sub_lines):
+                sys.exit()
+        else:
+            ind = (ind + 1) % len(sub_lines)
+            ls_text = ls_font.render(sub_lines[ind], True, FG_COLOR)
+        
     # --------
 
     pg.display.update()
